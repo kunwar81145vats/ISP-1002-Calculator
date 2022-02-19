a796eeea59822d5cd30ba84df423e95d3fd75159
kunwar81145vats <97743194+kunwar81145vats@users.noreply.github.com>
2022-02-19 18:02:14 +0530

    Commit details: 1. Addition implemented. 2. Subtraction implemented. 3. Multiplication implemented. 4. Division implemented. 5. Percentage implemented. 6. Clear screen implemented.

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -6,59 +6,132 @@
 //
 
 import UIKit
+import Foundation
 
 class ViewController: UIViewController {
 
     @IBOutlet weak var calculationLabel: UILabel!
     @IBOutlet weak var resultLabel: UILabel!
-
+    
+    /*
+     -1: none
+     0: addition
+     1: subtraction
+     2: multiplication
+     3: division
+     4: percentage
+     5: square root
+     6: cube root
+     7: log
+     8: +/-
+     9: power
+     */
+    var calculation: Int = -1
+    var prevNumber: String = ""
+    var currentNumber: String = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        uiSetup()
+        resetCalculator()
+    }
+    
+    func uiSetup()
+    {
+        calculationLabel.numberOfLines = 1
+        resultLabel.numberOfLines = 1
+        calculationLabel.lineBreakMode = .byTruncatingHead
+        resultLabel.lineBreakMode = .byTruncatingHead
     }
     
     @IBAction func numberButtonAction(_ sender: UIButton)
     {
-        switch sender.tag {
-        case 0:
-            print("0 pressed")
-        case 1:
-            print("1 pressed")
-        case 2:
-            print("2 pressed")
-        case 3:
-            print("3 pressed")
-        case 4:
-            print("4 pressed")
-        case 5:
-            print("5 pressed")
-        case 6:
-            print("6 pressed")
-        case 7:
-            print("7 pressed")
-        case 8:
-            print("8 pressed")
-        case 9:
-            print("9 pressed")
+        var enteredDigit: String = ""
+        
+        if sender.tag >= 0
+        {
+            enteredDigit = "\(sender.tag)"
+        }
+        else
+        {
+            if ((calculationLabel.text?.last?.isNumber) != nil)
+            {
+                enteredDigit = "."
+            }
+            else
+            {
+                enteredDigit = "0."
+            }
+        }
+        
+        if calculationLabel.text == "0"
+        {
+            calculationLabel.text = enteredDigit
+        }
+        else
+        {
+            if prevNumber.count != 0
+            {
+                if currentNumber.count == 0
+                {
+                    currentNumber = enteredDigit
+                }
+                else
+                {
+                    currentNumber += enteredDigit
+                }
+            }
             
-        default:
-            print("decimal pressed")
+            calculateResult()
+            calculationLabel.text?.append(enteredDigit)
         }
     }
     
     @IBAction func basicCalculationButtonAction(_ sender: UIButton)
     {
+        if calculationLabel.text?.count == 0
+        {
+            return
+        }
+        
+        if prevNumber.count == 0
+        {
+            prevNumber = calculationLabel.text ?? ""
+        }
+        else
+        {
+            prevNumber = resultLabel.text ?? ""
+        }
+        
+        currentNumber = ""
+        
         switch sender.tag {
         case 101:
             print("/ is pressed")
+            calculation = 3
+            calculationLabel.text?.append(" / ")
         case 102:
             print("X is pressed")
+            calculation = 2
+            calculationLabel.text?.append(" x ")
+
         case 103:
             print("- is pressed")
+            calculation = 1
+            calculationLabel.text?.append(" - ")
+
         case 104:
             print("+ is pressed")
+            calculation = 0
+            calculationLabel.text?.append(" + ")
+
         case 105:
             print("% is pressed")
+            calculation = 4
+            calculationLabel.text?.append(" % ")
+
+            
         default:
             print("will never get executed")
         }
@@ -66,23 +139,85 @@ class ViewController: UIViewController {
     
     @IBAction func complexCalculationButtonAction(_ sender: UIButton)
     {
-        
+        switch sender.tag {
+        case 201:
+            print("^ is pressed")
+        case 202:
+            print("square root is pressed")
+        case 203:
+            print("cube root is pressed")
+        case 204:
+            print("log is pressed")
+        default:
+            print("will never get executed")
+        }
     }
     
     @IBAction func clearButtonAction(_ sender: UIButton)
     {
-        resultLabel.text = ""
-        calculationLabel.text = ""
+        resetCalculator()
     }
     
     @IBAction func resultButtonAction(_ sender: UIButton)
     {
-        
+        calculationLabel.text = resultLabel.text
+        resultLabel.text = ""
+        calculation = -1
     }
     
     @IBAction func plusMinusButtonAction(_ sender: UIButton)
     {
         
+    }
+    
+    func calculateResult()
+    {
+        let leftVal: Float = Float(prevNumber) ?? 0.0
+        let rightVal: Float = Float(currentNumber) ?? 0.0
+        var result: Float = 0.0
+        
+        switch calculation {
+        case 0:
+            result = leftVal + rightVal
+        case 1:
+            result = leftVal - rightVal
+        case 2:
+            result = leftVal * rightVal
+        case 3:
+            result = leftVal / rightVal
+        case 4:
+            result = leftVal.truncatingRemainder(dividingBy: rightVal)
+        case 5:
+            calculationLabel.text = "2√(\(calculationLabel.text ?? ""))"
+            result = leftVal.squareRoot()
+        case 6:
+            calculationLabel.text = "3√(\(calculationLabel.text ?? ""))"
+            result = pow(leftVal, 1/3)
+        case 7:
+            print("log calculate")
+        case 8:
+            print("+/-")
+        case 9:
+            print("power")
+            
+        default:
+            print("do nothing")
+        }
+                
+        let formatter = NumberFormatter()
+        formatter.minimumFractionDigits = 0
+        formatter.maximumFractionDigits = 2
+        
+        resultLabel.text = "\(formatter.string(from: NSNumber(value: result)) ?? "")"
+    }
+    
+    func resetCalculator()
+    {
+        resultLabel.text = ""
+        calculationLabel.text = "0"
+        calculation = -1
+        prevNumber = ""
+        currentNumber = ""
     }
 }
 
